d7e3691d4c60807f35064cb4f2f7bd267c228bf4
SilvertK164 <157436430+SilvertK164@users.noreply.github.com>
2025-02-18 18:06:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ st.title('Resistencia a la Compresión del Concreto - ML 🤖🏗️')
 
 st.write('Esta aplicación fue creada a partir de tres modelo de Machine Learning')
 st.write('by: Silvert Kevin Quispe Pacompia')
+
+with st.expander('Data'):
+  st.write('**Data de Concreto 🏗️')
+  df = pd.read_csv('Concrete_Data.csv')
