210ce6bb344812051c6fd61da720d8900f29f85c
SilvertK164 <157436430+SilvertK164@users.noreply.github.com>
2025-02-18 23:15:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ with st.sidebar:
             <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" width="24" alt="LinkedIn" style="vertical-align: middle;"> LinkedIn
         </a><br>
         <a href="https://www.tiktok.com/@silvertk164" target="_blank">
-            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/0/07/TikTok_logo.svg/1024px-TikTok_logo.svg.png" width="24" alt="TikTok" style="vertical-align: middle;"> TikTok
+            <img src="https://upload.wikimedia.org/wikipedia/en/a/a9/TikTok_logo.svg" width="24" alt="TikTok" style="vertical-align: middle;"> TikTok
         </a><br>
         <a href="https://github.com/SilvertK164" target="_blank">
             <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" width="24" alt="GitHub" style="vertical-align: middle;"> GitHub
@@ -60,6 +60,7 @@ with st.sidebar:
         """,
         unsafe_allow_html=True
     )
+
     
     st.markdown("---")
     st.write('by: Silvert Kevin Quispe Pacompia')
